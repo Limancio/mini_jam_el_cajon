deba923ce8092ab6efaaf1beb6a7aa21dea4b01e
Limancio <luis.monteza@hotmail.com>
2023-07-02 19:01:31 -0300

    collision fixes

--- src/game/Player.java
+++ src/game/Player.java
@@ -13,6 +13,8 @@ import maths.vec3;
 public class Player extends Entity {
 	public RenderQuad quad;
 	
+	public boolean on_ground = true;
+	
 	public boolean OnTheGround=true;
 	public float time=0;
 	public int JumpsLeft=2;
@@ -31,19 +33,13 @@ public class Player extends Entity {
 		
 		if(window.is_key_press(GLFW_KEY_A)) {
 			motion.x -= player_speed * delta_time;
-		}
+		} 
 		if(window.is_key_press(GLFW_KEY_D)) {
 			motion.x += player_speed * delta_time;
-		}
-		if(window.is_key_press(GLFW_KEY_W)) {
-			motion.y += player_speed * delta_time;
-		}
-		if(window.is_key_press(GLFW_KEY_S)) {
-			motion.y -= player_speed * delta_time;
-		}
+		} 
 		
 		if(time<=0){
-			if (this.OnTheGround==true|| this.JumpsLeft==1) {
+			if (this.on_ground==true|| this.JumpsLeft==1) {
 				if(window.is_key_press(GLFW_KEY_SPACE)) {
 					time=0.5f;
 					
@@ -51,7 +47,7 @@ public class Player extends Entity {
 					System.out.println(this.JumpsLeft);
 					
 					this.JumpsLeft-=1;
-					this.OnTheGround=false;
+					this.on_ground=false;
 				}
 			}
 		}else {
@@ -61,49 +57,79 @@ public class Player extends Entity {
 	}
 
 	public void Character_Fall(Window window, float delta_time) {
-		if(window.is_key_press(GLFW_KEY_W)==false && time<=0) {
-			if(position.y>0) {
-				motion.y -= 200 * delta_time;
+		if(window.is_key_press(GLFW_KEY_W) == false && time <= 0) {
+			if(on_ground == false) {
+				motion.y -= 400 * delta_time;
+			} else {
+				motion.y = 0;
 			}
 		}
 	}
 	
 	public void update(Window window, Level level, float delta_time) {
-		Character_Fall(window, delta_time);
-		
-		boolean ignore_movement = false;
 		vec3 target_position = vec3.add(position, motion);
-
 		vec2 player_center = new vec2(target_position.x + (quad.rect.w * 0.5f) - (box.x * 0.5f), target_position.y + (quad.rect.h * 0.5f) - (box.y * 0.5f));
 		CollisionBox player_box = new CollisionBox(player_center.x, player_center.y, player_center.x + box.x, player_center.y + box.y);
-		
+
 		for(StaticObject it : level.blocks) {
 			if(it != null) {
 				if(it.block_type == 1) {
-					CollisionBox block_box  = new CollisionBox(it.position.x, it.position.y, 
+					CollisionBox block_box = new CollisionBox(it.position.x, it.position.y, 
 							it.position.x + it.quad.rect.w, it.position.y + it.quad.rect.h);
 					
+					
 					if(player_box.do_overlap(block_box)) {
-						ignore_movement = true;
+						if(motion.x > 0.0f) {
+							motion.x = 0f;
+						} 
+						if(motion.x < 0.0f) {
+							motion.x = 0f;
+						} 
+						
 						break;
 					}
 				}
 			}
 		}
+		
+		target_position = vec3.add(position, motion);
+		player_center	= new vec2(target_position.x + (quad.rect.w * 0.5f) - (box.x * 0.5f), target_position.y + (quad.rect.h * 0.5f) - (box.y * 0.5f));
+		player_box 		= new CollisionBox(player_center.x, player_center.y, player_center.x + box.x, player_center.y + box.y);
+		
+		on_ground = false;
+		for(StaticObject it : level.blocks) {
+			if(it != null && it.block_type == 1) {
+					CollisionBox check_ground_box = new CollisionBox(it.position.x, it.position.y + (it.quad.rect.h * 0.5f), 
+							it.position.x + it.quad.rect.w, it.position.y + (it.quad.rect.h * 1.01f));
+					
+					if(player_box.do_overlap(check_ground_box)) {
+						on_ground = true;
+						this.JumpsLeft=2;
+						break;
+					}
+			}
+		}
+		for(StaticObject it : level.blocks) {
+			if(it != null && it.block_type == 1) {
+				CollisionBox check_head_box = new CollisionBox(it.position.x, it.position.y - (it.quad.rect.h * 0.05f), 
+						it.position.x + it.quad.rect.w, it.position.y + it.quad.rect.h);
 
-		if(position.y<=0) {
-			this.OnTheGround=true;
-			this.JumpsLeft=2;
+				if(player_box.do_overlap(check_head_box)) {
+					motion.y = 0f;
+					time = 0;
+					break;
+				}
+			}
 		}
 		
-		if(ignore_movement == false) {
-			position = target_position;
-		}
+		Character_Fall(window, delta_time);
+		position = vec3.add(position, motion);
 	}
 	
 	public void render(ShaderProgram shader) {
 		mat4 model_matrix = new mat4(1.0f);
 		model_matrix.multiply(mat4.translate_matrix(position));
+		
 		shader.set_uniform("u_model_matrix", model_matrix);
 		quad.render(shader);
 	}
